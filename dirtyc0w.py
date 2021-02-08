59a98795cfaebb80fe8418fd1aa041a411d93253
Allen <allenylhan@gmail.com>
2021-02-08 17:23:15 -0500

    Update version requirements

--- dirtyc0w.py
+++ dirtyc0w.py
@@ -3,7 +3,7 @@
 """
 Welcome to the Dirty Cow lab!
 
-REQUIREMENT: >= python 3.6
+REQUIREMENT: == python 3.6
 
 NOTE: There are hints spread out through the file (base64 encoded). You can 
 decode these strings if you need a hint.
